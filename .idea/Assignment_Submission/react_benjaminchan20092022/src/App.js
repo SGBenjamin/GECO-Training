ed910110a2d40b783d8b010f70fcbbd3c9dafe85
Tat Sheng Benjamin Chan <Benjamin Chan>
2022-09-20 17:47:35 +0800

    20092022 React & Local/Sesson Storage Assignment

---
+++ .idea/Assignment_Submission/react_benjaminchan20092022/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my first React App.
+          <br>
+          </br>
+          Benjamin Chan
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          
+        </a>
+      </header>
+      <p>First React Project by Benjamin Chan</p>
+    </div>
+  );
+}
+
+export default App;
