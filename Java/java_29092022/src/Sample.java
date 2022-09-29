4a61b3c40cda2e2ce001cefd88a783079a69aa17
Benjamin Chan <88296118+SGBenjamin@users.noreply.github.com>
2022-09-29 12:57:09 +0800

    Add files via upload

---
+++ Java/java_29092022/src/Sample.java
@@ -0,0 +1,11 @@
+public class Sample {
+    //global variable
+    int num1 = 1233;
+    boolean isChild = true;
+
+    public static void main(String[] args) {
+        Sample sample = new Sample();
+        System.out.println(sample.num1);
+        System.out.println(sample.isChild);
+    }
+}
