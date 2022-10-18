b8fc4f41c3e886ca2de6bf20e467c81232d1f710
Benjamin Chan <benjaminchan85@gmail.com>
2022-10-18 20:45:26 +0800

    Daily Assignment
    Create a spring cloud -ok
    Create 2 spring client apps and connect with cloud. - ok
    Access the Apigateway url from the postman
    Implement the api gateway and circuit breaker

--- Java/src/springcloud/service1/src/main/java/com/springcloud/service1/Service1Application.java
+++ Java/src/springcloud/service1/src/main/java/com/springcloud/service1/Service1Application.java
@@ -2,8 +2,11 @@ package com.springcloud.service1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Service1Application {
 
 	public static void main(String[] args) {
